cf70b141dc52963a65643bf8deb170714e8ec3be
Herman De Beukelaer <herman.de.beukelaer@howest.be>
2022-05-03 13:29:38 +0200

    Move Coordinate struct to own code file

--- Prb.Structs.Wpf/MainWindow.xaml.cs
+++ Prb.Structs.Wpf/MainWindow.xaml.cs
@@ -58,42 +58,5 @@ namespace Prb.Structs.Wpf
             currentPosition = currentPosition.MoveRight();
             ShowPosition();
         }
-    }
-
-    struct Coordinate
-    {
-        public readonly int x;
-        public readonly int y;
-
-        public Coordinate(int x, int y)
-        {
-            this.x = x;
-            this.y = y;
-        }
-
-        public Coordinate MoveUp()
-        {
-            return new Coordinate(x, y + 1);
-        }
-
-        public Coordinate MoveDown()
-        {
-            return new Coordinate(x, y - 1);
-        }
-
-        public Coordinate MoveLeft()
-        {
-            return new Coordinate(x - 1, y);
-        }
-
-        public Coordinate MoveRight()
-        {
-            return new Coordinate(x + 1, y);
-        }
-
-        public override string ToString()
-        {
-            return  $"({x}, {y})";
-        }
-    }
+    } 
 }
